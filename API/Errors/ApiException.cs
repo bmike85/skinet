20ecb830d4dee3a25d28128a7f198777d298bd0f
bmike85 <m.biebel85@gmail.com>
2020-10-13 15:32:49 +0200

    End of Section 5

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace API.Errors
+{
+    public class ApiException : ApiResponse
+    {
+        public string Details { get; set; }
+        public ApiException(int statusCode, string message = null, string details = null) : base(statusCode, message)
+        {
+            Details = details;
+        }
+    }
+}
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,15 +1,13 @@
-using System;
+using API.Extensions;
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
-using Core.Entities;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace API
 {
@@ -29,18 +27,19 @@ namespace API
         {
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+
             services.AddAutoMapper(typeof(MappingProfiles));
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
+
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
@@ -49,6 +48,8 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseSwaggerDocumentation();
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
